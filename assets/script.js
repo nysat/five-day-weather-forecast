065a2c323b0078306ccfae74837d82c5caff9e34
nysat <nysa-cere3@hotmail.com>
2023-08-10 23:26:26 -0400

    added a function and tried to link the API to the webpage

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,35 @@
-for (var i = 0; i < weatherForecast.list.length; i+=8) {
-    console.log(weatherForecast.list[i].dt_txt);
-  }
+
+function getForecast (){
+    fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid==5c7e7da63239bdeab621911b842b41f7`)
+    .then (response => {
+        return response.json()
+    })
+    .then (response => {
+        lat =(response[0].lat);
+        lon = (response[0].lon);
+        console.log(response)
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var i = 0; i < weatherForecast.list.length; i+=8) {
+//     console.log(weatherForecast.list[i].dt_txt);
+//   }
